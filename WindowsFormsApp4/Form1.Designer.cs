44dd6825f9e2bc1932003bf6e6ee239a0e59b620
User <User@DESKTOP-RBMPGAB>
2023-10-04 16:02:58 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.нахуйСнестиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.созданиеКаталогаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "меню ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.создатьToolStripMenuItem,
+            this.открытьToolStripMenuItem,
+            this.нахуйСнестиToolStripMenuItem,
+            this.созданиеКаталогаToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 136);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.сохранитьToolStripMenuItem.Text = "сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.SaveMenuItem_Click);
+            // 
+            // создатьToolStripMenuItem
+            // 
+            this.создатьToolStripMenuItem.Name = "создатьToolStripMenuItem";
+            this.создатьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.создатьToolStripMenuItem.Text = "создать";
+            this.создатьToolStripMenuItem.Click += new System.EventHandler(this.CreateFileMenuItem_Click);
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.открытьToolStripMenuItem.Text = "открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // нахуйСнестиToolStripMenuItem
+            // 
+            this.нахуйСнестиToolStripMenuItem.Name = "нахуйСнестиToolStripMenuItem";
+            this.нахуйСнестиToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.нахуйСнестиToolStripMenuItem.Text = "снетси";
+            this.нахуйСнестиToolStripMenuItem.Click += new System.EventHandler(this.удалитьФайлToolStripMenuItem_Click);
+            // 
+            // созданиеКаталогаToolStripMenuItem
+            // 
+            this.созданиеКаталогаToolStripMenuItem.Name = "созданиеКаталогаToolStripMenuItem";
+            this.созданиеКаталогаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.созданиеКаталогаToolStripMenuItem.Text = "создание каталога";
+            this.созданиеКаталогаToolStripMenuItem.Click += new System.EventHandler(this.CreateDirectoryMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(768, 373);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem создатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem нахуйСнестиToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem созданиеКаталогаToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Point point = new Point();
+            point = this.Location;
+            point.X += 8;
+            point.Y += 32;
+            contextMenuStrip1.Show(point);
+            
+        }
+
+        private void CreateDirectoryMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string folderPath = folderBrowserDialog.SelectedPath;
+                string directoryName = "МойКаталог"; // Установите желаемое имя каталога
+
+                try
+                {
+                    string fullDirectoryPath = Path.Combine(folderPath, directoryName);
+
+                    // Проверяем, существует ли каталог
+                    if (!Directory.Exists(fullDirectoryPath))
+                    {
+                        // Создаем каталог
+                        Directory.CreateDirectory(fullDirectoryPath);
+                        MessageBox.Show($"Каталог успешно создан: {fullDirectoryPath}");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Каталог уже существует.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Произошла ошибка при создании каталога: {ex.Message}");
+                }
+            }
+        }
+
+        private void создатьКаталогToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string folderPath = folderBrowserDialog.SelectedPath;
+
+                try
+                {
+                    // Проверяем, существует ли каталог
+                    if (!Directory.Exists(folderPath))
+                    {
+                        // Создаем каталог
+                        Directory.CreateDirectory(folderPath);
+                        MessageBox.Show($"Каталог успешно создан: {folderPath}");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Каталог уже существует.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Произошла ошибка при создании каталога: {ex.Message}");
+                }
+            }
+        }
+
+        private void удалитьФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Установите фильтр файлов, если нужно
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                try
+                {
+                    // Проверяем, существует ли файл
+                    if (File.Exists(filePath))
+                    {
+                        // Удаляем файл
+                        File.Delete(filePath);
+                        MessageBox.Show($"Файл успешно удален: {filePath}");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Файл не существует.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Произошла ошибка при удалении файла: {ex.Message}");
+                }
+            }
+        }
+
+        private void ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Установите фильтр файлов, если нужно
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                try
+                {
+                    // Открываем выбранный файл и отображаем его содержимое в текстовом поле
+                    textBox1.Text = File.ReadAllText(filePath);
+                    MessageBox.Show($"Файл успешно открыт: {filePath}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Произошла ошибка при открытии файла: {ex.Message}");
+                }
+            }
+        }
+
+        private void CreateFileMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            // Установите фильтр файлов, если нужно
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                try
+                {
+                    // Создаем новый файл (если он не существует) и открываем его для редактирования
+                    File.Create(filePath).Close();
+
+                    // Открываем созданный файл в текстовом поле
+                    textBox1.Text = File.ReadAllText(filePath);
+                    MessageBox.Show($"Создан новый файл: {filePath}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Произошла ошибка при создании файла: {ex.Message}");
+                }
+            }
+        }
+
+        private void SaveMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            // Установите фильтр файлов, если нужно
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                string textToSave = textBox1.Text;
+
+                try
+                {
+                    // Сохраняем текст в выбранный файл
+                    File.WriteAllText(filePath, textToSave);
+                    
+                    MessageBox.Show("Текст успешно сохранен.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Произошла ошибка при сохранении: {ex.Message}");
+                }
+            }
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
